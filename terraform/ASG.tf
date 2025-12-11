63f132869c670d5dc159e50b54732611d0c8e605
kurosu <ggosaoao@gmail.com>
2025-12-11 20:15:08 +0200

    add scaling cpu-policy

--- terraform/ASG.tf
+++ terraform/ASG.tf
@@ -29,3 +29,17 @@ resource "aws_autoscaling_group" "app_asg" {
     version = "$Latest"
   }
 }
+
+# Scaling Policy
+resource "aws_autoscaling_policy" "cpu_policy" {
+  name                   = "cpu-target-tracking"
+  autoscaling_group_name = aws_autoscaling_group.app_asg.name
+  policy_type            = "TargetTrackingScaling"
+
+  target_tracking_configuration {
+    predefined_metric_specification {
+      predefined_metric_type = "ASGAverageCPUUtilization"
+    }
+    target_value = 70.0
+  }
+}
